75f8870492ff9c1f4ddc30eb393db829cb4c3922
bhupenderstrix-lang <bhupender.strix@gmail.com>
2025-10-03 10:38:22 +0530

    The d

---
+++ Funtions/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * multi line comment
+ * this is
+ */
+// single line comment
+#singe line comment
+
+
+//int
+echo "<h2>Develop</h2>";
+
+
+
+$a=4;
+$b=3;
+$c= $a +$b;
+echo $c;
+?>
+<?php
+//second 
+echo "<h2>Hide something</h2>";
+
+
+$a=12; //integer
+$b=1.5; #float
+$c='32'; //string
+$d= true; #boolean
+$e =$a+$b+$c+$d;
+echo $e; // output 
+?>
